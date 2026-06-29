7a7673f6d52e539ef45a53393388f659afeeeb16
Robin Jarry <robin@jarry.cc>
2026-06-29 15:24:00 +0200

    admin: add project management
    Add "pw admin project" subcommands for creating, listing, editing
    and deleting projects. Support all project fields including list
    ID, subject match patterns and URL templates.
    
    Signed-off-by: Robin Jarry <robin@jarry.cc>

---
+++ cmd/pw/admin/main.go
@@ -0,0 +1,10 @@
+// Patchwork - automated patch tracking system
+// Copyright (C) The Patchwork Contributors (see CONTRIBUTORS)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package admin
+
+type CLI struct {
+	Project ProjectCmd `cmd:"" help:"Manage projects."`
+}
